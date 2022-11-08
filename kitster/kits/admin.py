9368ce631b80d807877a01cfabefaaf18230ef7c
Evgeni <bikovevg@gmail.com>
2022-11-08 17:48:57 +0500

    add product inlines to kit admin

--- kitster/kits/admin.py
+++ kitster/kits/admin.py
@@ -4,9 +4,19 @@ from django.contrib import admin
 from .models import Author, Kit, Product, Tag, KitHit
 
 admin.site.register(Author)
-admin.site.register(Kit)
 admin.site.register(Product)
 admin.site.register(Tag)
+
+class ProductInline(admin.TabularInline):
+    model = Product
+
+class KitAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductInline,
+    ]
+
+admin.site.register(Kit, KitAdmin)
+
 
 class KitHitAdmin(admin.ModelAdmin):
     list_display= ('pub_date', 'kit', 'ip')
