f1bc6405e3f91053690048f93dac0f5567588fbf
Evgeni <bikovevg@gmail.com>
2022-11-08 17:59:19 +0500

    other link added to product model

--- kitster/kits/models.py
+++ kitster/kits/models.py
@@ -42,7 +42,7 @@ class Product(models.Model):
 	description = models.CharField(max_length=200)
 	img_url = models.FileField(upload_to='static', max_length=255)
 	yandex_market_link = models.CharField(max_length=200)
-	# yandex_market_link = models.CharField(max_length=200)
+	other_link = models.CharField(max_length=200, blank=True, null=True)
 
 	def __str__(self):
 		return self.name
